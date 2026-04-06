e600bb18152647600624b631f0b7f4e222dc4629
Atticus Finley <71795010+beackers@users.noreply.github.com>
2026-04-06 06:33:43 -0500

    get MainActivity set up

--- app/src/main/java/com/beackers/pass/MainActivity.kt
+++ app/src/main/java/com/beackers/pass/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.beackers.pass
 
 import android.os.Bundle
+import android.content.Intent
+
 import androidx.appcompat.app.AppCompatActivity
+import com.beackers.pass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflator)
+        setContentView(binding.root)
+
+        binding.armButton.setOnClickListener {
+          val intent = Intent(this, AlarmActivity::class.java)
+          startActivity(intent)
+        }
+
+        binding.settingsButton.setOnClickListener {
+          val intent = Intent(this, SettingsActivity::class.java)
+          startActivity(intent)
+        }
     }
 }
