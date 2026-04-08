0235b6a8682a0127428678d39316ad00bba16b81
Atticus Finley <71795010+beackers@users.noreply.github.com>
2026-04-07 20:30:11 -0500

    add coroutines

--- app/src/main/java/com/beackers/pass/alarm/AlarmService.kt
+++ app/src/main/java/com/beackers/pass/alarm/AlarmService.kt
@@ -10,21 +10,53 @@ import android.app.NotificationManager
 
 import androidx.core.app.NotificationCompat
 
+import kotlinx.coroutines.*
+
 class AlarmService : Service() {
+  // scope declaration
+  private val serviceScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
+  // PASS state management
+  //private var measureJob: Job? = null
   private var state = AlarmState.STANDBY
+  private var pressureWindow = ArrayDeque<Float>()
+  private const val WINDOW_SIZE = 25
+  private const val MOVEMENT_THRESHOLD = 0.08f
+  private lateinit var sensorManager: SensorManager
+  private var pressureSensor: Sensor? = null
+  private var pressureListener = object : SensorEventListener {
+    override fun onSensorChanged(event: SensorEvent) {
+      val pressure = event.values[0]
+      handlePressure(pressure)
+    }
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+  }
 
+  // timer
+  private var timerJob: Job? = null
+  private var idleSeconds = 0
+  
   override fun onBind(intent: Intent?) = null
 
   override fun onCreate() {
     super.onCreate()
     startForeground(1, buildNotification())
-    // TODO: start barometer window and timers
+    startTimer() // start timer, regardless
+    sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+    if (pressureSensor != null) {
+      sensorManager.registerListener(
+        pressureListener,
+        pressureSensor,
+        200_000
+      )
+    }
   }
 
   override fun onDestroy() {
     super.onDestroy()
-    // TODO: clean up timers
+    serviceScope.cancel() // clear timers
+    sensorManager.unregisterListener(pressureListener)
   }
 
   override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -58,13 +90,11 @@ class AlarmService : Service() {
   private fun arm() {
     if (state != AlarmState.STANDBY) return
     state = AlarmState.ARMED
-    // TODO startTimer()
     // TODO AlarmNoise.ack()
   }
 
   private fun disarm() {
     state = AlarmState.STANDBY
-    // TODO stopTimer()
     // TODO AlarmSound.stop()
   }
 
@@ -83,9 +113,55 @@ class AlarmService : Service() {
   private fun reset() {
     if (state == AlarmState.PREALARM || state == AlarmState.ALARMING) {
       state = AlarmState.ARMED
-      // TODO resetTimer()
+      registerMovement()
       // TODO AlarmSound.stop()
       // TODO AlarmSound.ack()
+    }
+  }
+
+
+  // count until death
+  private fun startTimer() {
+    if (timerJob?.isActive == true) return
+    seconds = 0
+    timerJob = serviceScope.launch {
+      while (isActive) {
+        delay(1000)
+        idleSeconds++
+        onTick()
+      }
+    }
+  }
+
+  // reset if moved
+  fun registerMovement() {
+    idleSeconds = 0
+  }
+
+  // tick -> check if he's down
+  private suspend fun onTick() {
+    withContext(Dispatchers.Main) {
+      when {
+        idleSeconds == 20 && state == AlarmState.ARMED -> prealarm()
+        idleSeconds == 30 && state == AlarmState.PREALARM -> alarm()
+      }
+    }
+  }
+
+  private fun handlePressure(pressure: Float) {
+    if (state == AlarmState.STANDBY) return
+    pressureWindow.addLast(pressure)
+    if (pressureWindow.size > WINDOW_SIZE) {
+      pressureWindow.removeFirst()
+    }
+    if (pressureWindow.size == WINDOW_SIZE) {
+      val min = pressureWindow.minOrNull()!!
+      val max = pressureWindow.maxOrNull()!!
+      val range = max - min
+
+      if (range > MOVEMENT_THRESHOLD) {
+        registerMovement()
+      }
     }
   }
 }
